5afe550efb78ce6893e5c6c24c836eb3ae06a265
Vladix97 <gramovv@gmail.com>
2017-10-21 23:16:58 +0300

    refactoring ...

--- ExpressJS/ExamPreparation02/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    development: {
-        port: process.env.PORT || 3000,
-        dbPath: 'mongodb://localhost:27017/the-exam'
-    },
-    production: {  }
-}
--- ExpressJS/ExamPreparation02/util/encryption.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto')
-
-module.exports = {
-    generateSalt: () =>
-        crypto.randomBytes(128).toString('base64'),
-    generateHashedPassword: (salt, password) =>
-        crypto.createHmac('sha256', salt).update(password).digest('hex')
-}
