90414d510e268e300615b0d1a63fe013b5c79b5f
Vladix97 <gramovv@gmail.com>
2017-11-10 22:51:25 +0200

    routing - gallery app

---
+++ Angular/CodeCademy/Routing/js/services/photo.js
@@ -0,0 +1,10 @@
+app.factory('photos', ['$http', function($http) {
+  return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json')
+         .success(function(data) {
+           return data;
+         })
+         .error(function(data) {
+           return data;
+         });
+}]);
+
