bb3701a707b54053103189555d3631d994286612
Vladix97 <gramovv@gmail.com>
2017-12-02 00:29:59 +0200

    *ngFor

--- Angular/TourOfHeroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Angular/TourOfHeroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -13,10 +14,17 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
+  heroes = HEROES;
+
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
